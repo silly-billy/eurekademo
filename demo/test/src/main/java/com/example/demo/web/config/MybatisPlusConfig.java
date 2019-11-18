065d18f3205cab0606552f1ed1f240053f1384fb
silly-billy <1579700874@qq.com>
2019-11-18 08:36:47 +0800

    modify

--- demo/test/src/main/java/com/example/demo/web/config/MybatisPlusConfig.java
+++ demo/test/src/main/java/com/example/demo/web/config/MybatisPlusConfig.java
@@ -1,12 +1,4 @@
-/** 
- * All rights Reserved, Designed By www.maihaoche.com
- *
- * @Package com.mhc.pandora.core.base.config
- * @author: 三帝（sandi@maihaoche.com）
- * @date: 2018-12-18 11:00:45
- * @Copyright: 2017-2020 www.maihaoche.com Inc. All rights reserved.
- * 注意：本内容仅限于卖好车内部传阅，禁止外泄以及用于其他的商业目
- */
+
 package com.example.demo.web.config;
 
 import com.baomidou.mybatisplus.mapper.ISqlInjector;
